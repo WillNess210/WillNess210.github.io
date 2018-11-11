1839d58eb1eefe65bf4965104121378487885277
WillNess210 <WillNess210@gmail.com>
2018-11-11 12:44:58 -0700

    links, contact info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
 $(function() {
+	// IF ODD # PROJECTS, MAKE FIRST PROJECT TAKE UP 2 COLUMNS
 	var projectCount = $(".project").length;
 	if(projectCount % 2 == 1){
 		$(".project").first().css("grid-column", "span 2");
 	}
+	// ANIMATE CONTACT BARS
+	$(".contact").on("mouseover", function(){
+		$(this).css("backgroundColor", "#606060");
+	});
+	$(".contact").on("mouseout", function(){
+		$(this).css("backgroundColor", "#454545");
+	});
+	// CLICK ON WHOLE DIV
+	$(".contact").click(function() {
+	  window.location = $(this).find("a").attr("href"); 
+	  return false;
+	});
+	$(".project").click(function() {
+		if($(this).data("link")){
+			window.location = $(this).data("link");
+		}
+	  return false;
+	});
+	// REMOVE MY NAME AFTER 5 SECONDS
+	setTimeout(function(){
+        $('#myname').slideToggle("slow");
+    }, 5000);
 });
